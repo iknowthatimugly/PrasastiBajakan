544ec2f6dcb83190311966ec40ac0657c595b8c0
Chester <chesterlynx221@gmail.com>
2019-08-01 16:50:10 +0700

    tugas pertama

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$kg = 15000;
+$dus = 2000;
+$hasilkg = ($kg * 6);
+$hasildus = ($dus * 6);
+$total = $hasilkg + $hasildus;
+echo "Diketahui!<br>";
+echo "1 kg = 15,000<br>";
+echo "1 dus = 2,000<br>";
+echo "Ditoko terdapat 6 dus setiap 1 dus 5kg,Berapa hasil penjualanya??<br>";
+echo "Harga penjualan!<br>";
+echo "6 x 15,000 = $hasilkg<br>";
+echo "6 x 2,000 = $hasildus<br>";
+echo "Total penjualan = $total<br>";
+?>
---
+++ index2.php
@@ -0,0 +1,19 @@
+<?php
+    $tama = 32;
+    $toni = (0.5 * 32);
+    $tini = 32 - 14;
+
+    echo "Jika umur Toni setengah dari umur Tama,<br> umur Tini 14 tahun lebih muda dari Tama.<br> Dan jumlah umur Toni dan Tini adalah 34 tahun,<br>berpakah umur Toni, Tama, dan Tini?";
+    echo "diketahui!<br>";
+    echo "Umur tama = x<br>";
+    echo "umur toni =0,5 x umur tama<br>";
+    echo "umur tini = x -14<br>";
+    echo "umur toni + umur tini = 34<br>";
+    echo "0,5x + x-14 = 34<br>";
+    echo "1,5x = 34 + 14<br>";
+    echo "1,5x = 48<br>";
+    echo "x = 48 : 1,5 = 32<br>";
+    echo "Umur tama = $tama<br>";
+    echo "Umur toni = $toni<br>";
+    echo "Umur tini = $tini<br>";
+?>
