3af990e8076ca804c988d2660d6bd7825376d858
Chester <chesterlynx221@gmail.com>
2019-08-02 16:48:11 +0700

    New Changes

--- tugas.php
+++ tugas.php
@@ -9,51 +9,52 @@
 <body>
 <FORM ACTION="" METHOD="POST" NAME="input">
 Masukan angka : <input type="text" name="angka">
+<input type='submit' name='input' value='Submit'>
 </body>
 </html>
 <?php
-if (isset($_GET['input'])) {
+if (isset($_POST['input'])) {
     $angka = $_POST['angka'];
-    echo "anda memasukan angka $angka";
+    echo "<br>anda memasukan angka $angka";
     if ($angka == "1") {
-        echo "anda memasukan warna biru";
-        echo "<hr width=10px color='blue' >";
+        echo "<br>anda memasukan warna biru";
+        echo "<hr width=100px height=100vn color='blue' >";
     }
     elseif ($angka == "2") {
-        echo "anda memasukan warna merah";
-        echo "<hr width=10px color='red' >";
+        echo "<br>anda memasukan warna merah";
+        echo "<hr width=100px height=100vn color='red' >";
     }
     elseif ($angka == "3") {
-        echo "anda memasukan warna hijau";
-        echo "<hr width=10px color='green' >";
+        echo "<br>anda memasukan warna hijau";
+        echo "<hr width=100px height=100vn color='green' >";
     }
     elseif ($angka == "4") {
-        echo "anda memasukan warna kuning";
-        echo "<hr width=10px color='yellow' >";
+        echo "<br>anda memasukan warna kuning";
+        echo "<hr width=100px height=100vn color='yellow' >";
     }
     elseif ($angka == "5") {
-        echo "anda memasukan warna biru";
-        echo "<hr width=10px color='blue' >";
+        echo "<br>anda memasukan warna biru";
+        echo "<hr width=100px height=100vn color='blue' >";
     }
     elseif ($angka == "6") {
-        echo "anda memasukan warna biru";
-        echo "<hr width=10px color='blue' >";
+        echo "<br>anda memasukan warna biru";
+        echo "<hr width=100px height=100vn color='blue' >";
     }
     elseif ($angka == "7") {
-        echo "anda memasukan warna nigga";
-        echo "<hr width=10px color='#000000' >";
+        echo "<br>anda memasukan warna nigga";
+        echo "<hr width=100px height=100vn color='#000000' >";
     }
     elseif ($angka == "8") {
-        echo "anda memasukan warna orange";
-        echo "<hr width=10px color='#ff8000' >";
+        echo "<br>anda memasukan warna orange";
+        echo "<hr width=100px height=100vn color='#ff8000' >";
     }
     elseif ($angka == "9") {
-        echo "anda memasukan warna pink";
-        echo "<hr width=10px color='#ff00ff' >";
+        echo "<br>anda memasukan warna pink";
+        echo "<hr width=100px height=100vn color='#ff00ff' >";
     }
     elseif ($angka == "10") {
-        echo "anda memasukan warna lime";
-        echo "<hr width=10px color='#66ff33' >";
+        echo "<br>anda memasukan warna lime";
+        echo "<hr width=100px height=100vn color='#66ff33' >";
     }
     else {
         echo "Maaf,Nomor yang anda masukan Invalid karena anda wibu";
